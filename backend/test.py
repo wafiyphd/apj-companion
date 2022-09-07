1cc9741fd53d22b03df7d2f0052ea8b5cb6bbaea
Wafiy Damit <wafiyphd@gmail.com>
2022-09-07 16:47:21 +0800

    Updated test route

--- backend/test.py
+++ backend/test.py
@@ -6,10 +6,10 @@ from pathlib import Path
 class Test(Resource):
   def get(self):
     current_dir = Path(__file__)
-    project_dir = [p for p in current_dir.parents if p.parts[-1]=='apj-companion'][0]
+    #project_dir = [p for p in current_dir.parents if p.parts[-1]=='apj-companion'][0]
     return {
       'resultStatus': 'success',
       'message': "Hello! This is the mini APJC Web App",
       'test_current': str(current_dir),
-      'test_project': str(project_dir)
+      #'test_project': str(project_dir)
       }
